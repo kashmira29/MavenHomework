45a762c1dd9d70fc5a7cc23055c375a304981811
kashmira29 <kashmirapandar@gmail.com>
2019-09-18 18:56:57 +0100

    fourth commit

--- src/main/java/com/demoNopeCommerce/Utils.java
+++ src/main/java/com/demoNopeCommerce/Utils.java
@@ -1,8 +1,13 @@
 package com.demoNopeCommerce;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,43 +18,51 @@ import java.util.concurrent.TimeUnit;
 public class Utils<idValue> extends BasePage {
 
     //method to findElement(By, by) and click() to Click on any element of the webpage
-    public void clickButton(By by) {
+    public void clickButton(By by)
+    {
         driver.findElement(by).click();
     }
 
     //findElement(By, by) with sendKeys()
-    public void enterText(By by, String text) {
+    public void enterText(By by, String text)
+    {
         driver.findElement(by).sendKeys(text);
     }
 
     //getText() is a method that gets you the inner text of the web element
-    public String getText(By by) {
+    public String getText(By by)
+    {
         return driver.findElement(by).getText();
     }
 
     //if the element to be clicked is a submit button
-    public void submit(By by) {
-
+    public void submit(By by)
+    {
         driver.findElement(by).submit();
     }
 
     // get() to open a URL in the current browser
-    public void openWeb(String url) {
+    public void openWeb(String url)
+    {
         driver.get(url);
     }
 
     //method to maximise the screen
-    public void maximisescreen() {
+    public void maximisescreen()
+
+    {
         driver.manage().window().fullscreen();
     }
 
     //method to navigate back
-    public void navigateBack() {
+    public void navigateBack()
+    {
         driver.navigate().back();
     }
 
     //method to navigate forward
-    public void navigateForward() {
+    public void navigateForward()
+    {
         driver.navigate().forward();
     }
 
@@ -100,23 +113,47 @@ public class Utils<idValue> extends BasePage {
 
     }
     //navigate to url
-    public void navigate(String url){
+    public void navigate(String url)
+    {
         driver.navigate().to(url);
     }
     //to get all elements in List
-    public List<WebElement> getskuList(By by) {
+    public List<WebElement> getskuList(By by)
+    {
         return driver.findElements(by);
     }
+
     //method to close current browser
     public void closeCurrentWeb(){
         driver.close();
     }
+
     //method to delete all cookies
     public void deleteCookies(){
         driver.manage().deleteAllCookies();
     }
 
+    //wait for clickable
+    public static void waitForclickable(By by,long time){
+        WebDriverWait wait = new WebDriverWait(driver,time);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+    //wait for element visible
+    public static void waitForelementVisible(By by,long time){
+        WebDriverWait wait = new WebDriverWait(driver,time);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+    }
+    //checking web element displayed or not
+    public boolean isElementDisplayed(By by)
+    {
+        return driver.findElement(by).isDisplayed();
+    }
 
+    //checking webelement is enable
+    public boolean ElementEnable(By by)
+    {
+        return driver.findElement(by).isEnabled();
+    }
 
 }
 
